dae0d64a2ac5d18c10d79fc0ab49554e93ad85e2
Gisle Nes <gisle@gisle.net>
2023-09-18 11:36:28 +0200

    Remove ElasticsearchRepositories config

--- src/main/java/no/einnsyn/apiv3/ApiV3Application.java
+++ src/main/java/no/einnsyn/apiv3/ApiV3Application.java
@@ -3,18 +3,11 @@ package no.einnsyn.apiv3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.FilterType;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
-import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
 @EnableAutoConfiguration
-@EnableJpaRepositories(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE,
-		value = ElasticsearchRepository.class))
-@EnableElasticsearchRepositories(includeFilters = @ComponentScan.Filter(
-		type = FilterType.ASSIGNABLE_TYPE, value = ElasticsearchRepository.class))
+@SpringBootApplication
+@EnableJpaRepositories
 public class ApiV3Application {
 
 	public static void main(String[] args) {
