c3a03b8cc8fbe3515e20848110c3bea84c3c4acd
Gisle Nes <gisle@gisle.net>
2023-09-29 11:25:32 +0200

    Added request / response objects required for Controllers

---
+++ src/main/java/no/einnsyn/apiv3/requests/GetSingleRequestParameters.java
@@ -0,0 +1,15 @@
+package no.einnsyn.apiv3.requests;
+
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class GetSingleRequestParameters {
+
+  private String externalId;
+
+  private List<String> expand;
+
+}
