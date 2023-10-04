9aff8af5e25c15b77f0bc531178138339d7bd22b
Gisle Nes <gisle@gisle.net>
2023-10-04 15:36:56 +0200

    Remove redundant import

--- src/main/java/no/einnsyn/apiv3/entities/enhet/models/EnhetJSON.java
+++ src/main/java/no/einnsyn/apiv3/entities/enhet/models/EnhetJSON.java
@@ -5,7 +5,6 @@ import java.util.List;
 import jakarta.persistence.Column;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
-import jakarta.persistence.ManyToOne;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
