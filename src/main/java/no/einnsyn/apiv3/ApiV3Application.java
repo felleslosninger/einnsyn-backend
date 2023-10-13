f4f906af15707e990aef11225b34b4681183238d
Gisle Nes <gisle@gisle.net>
2023-10-13 10:20:51 +0200

    Add Integrasjonspunkt-config

--- src/main/java/no/einnsyn/apiv3/ApiV3Application.java
+++ src/main/java/no/einnsyn/apiv3/ApiV3Application.java
@@ -3,11 +3,13 @@ package no.einnsyn.apiv3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableAutoConfiguration
 @SpringBootApplication
 @EnableJpaRepositories
+@ComponentScan("no.einnsyn")
 public class ApiV3Application {
 
 	public static void main(String[] args) {
