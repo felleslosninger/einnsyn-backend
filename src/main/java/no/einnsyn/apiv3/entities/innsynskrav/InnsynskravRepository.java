d4777b8ffe07abaa5d33fc3b36ed1802fae86884
Gisle Nes <gisle@gisle.net>
2023-10-25 10:06:19 +0200

    Add query to get Innsynskrav with failed InnsynskravDels

--- src/main/java/no/einnsyn/apiv3/entities/innsynskrav/InnsynskravRepository.java
+++ src/main/java/no/einnsyn/apiv3/entities/innsynskrav/InnsynskravRepository.java
@@ -1,9 +1,27 @@
 package no.einnsyn.apiv3.entities.innsynskrav;
 
+import java.time.Instant;
 import java.util.UUID;
+import java.util.stream.Stream;
+import org.springframework.data.jpa.repository.Query;
 import no.einnsyn.apiv3.entities.EinnsynRepository;
 import no.einnsyn.apiv3.entities.innsynskrav.models.Innsynskrav;
 
 public interface InnsynskravRepository extends EinnsynRepository<Innsynskrav, UUID> {
+
+  @Query("""
+        SELECT i
+        FROM Innsynskrav i
+        INNER JOIN i.innsynskravDel id
+        WHERE i.verified = true
+        AND id.sent IS NULL
+        AND id.retryCount < 6
+        AND (
+          id.retryTimestamp IS NULL OR
+          id.retryTimestamp < :compareTimestamp
+        )
+        GROUP BY i
+      """)
+  public Stream<Innsynskrav> findFailedSendings(Instant compareTimestamp);
 
 }
