18b65428b988ae3f62af744a128ddbe134540e6e
Daniel Lerum <daniel.lerum@digdir.no>
2023-11-29 09:25:39 +0100

    EIN-4504 test remove unused import

--- src/main/java/no/einnsyn/apiv3/entities/tilbakemelding/TilbakemeldingRepository.java
+++ src/main/java/no/einnsyn/apiv3/entities/tilbakemelding/TilbakemeldingRepository.java
@@ -1,10 +1,7 @@
 package no.einnsyn.apiv3.entities.tilbakemelding;
 
 import no.einnsyn.apiv3.entities.EinnsynRepository;
-import no.einnsyn.apiv3.entities.enhet.models.Enhet;
 import no.einnsyn.apiv3.entities.tilbakemelding.models.Tilbakemelding;
-
-import java.util.UUID;
 
 public interface TilbakemeldingRepository extends EinnsynRepository<Tilbakemelding, Long> {
 }
