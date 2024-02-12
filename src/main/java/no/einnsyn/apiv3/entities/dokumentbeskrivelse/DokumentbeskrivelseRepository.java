d1265e604aa299e92263095f92358db6498d48ca
Gisle Nes <gisle@gisle.net>
2024-02-12 15:35:28 +0100

    Add paginate by dokumentbeskrivelse

--- src/main/java/no/einnsyn/apiv3/entities/dokumentbeskrivelse/DokumentbeskrivelseRepository.java
+++ src/main/java/no/einnsyn/apiv3/entities/dokumentbeskrivelse/DokumentbeskrivelseRepository.java
@@ -3,6 +3,7 @@ package no.einnsyn.apiv3.entities.dokumentbeskrivelse;
 import no.einnsyn.apiv3.entities.arkivbase.ArkivBaseRepository;
 import no.einnsyn.apiv3.entities.dokumentbeskrivelse.models.Dokumentbeskrivelse;
 import no.einnsyn.apiv3.entities.journalpost.models.Journalpost;
+import no.einnsyn.apiv3.entities.moetedokument.models.Moetedokument;
 import no.einnsyn.apiv3.entities.moetesak.models.Moetesak;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -11,13 +12,13 @@ import org.springframework.data.jpa.repository.Query;
 public interface DokumentbeskrivelseRepository extends ArkivBaseRepository<Dokumentbeskrivelse> {
 
   @Query(
-      "SELECT d FROM Dokumentbeskrivelse d JOIN d.journalpost j WHERE j = :journalpost AND (:pivot"
-          + " IS NULL OR d.id >= :pivot) ORDER BY d.id ASC")
+      "SELECT d FROM Dokumentbeskrivelse d JOIN d.journalpost j WHERE j = :journalpost AND"
+          + " (:pivot IS NULL OR d.id >= :pivot) ORDER BY d.id ASC")
   Page<Dokumentbeskrivelse> paginateAsc(Journalpost journalpost, String pivot, Pageable pageable);
 
   @Query(
-      "SELECT d FROM Dokumentbeskrivelse d JOIN d.journalpost j WHERE j = :journalpost AND (:pivot"
-          + " IS NULL OR d.id <= :pivot) ORDER BY d.id DESC")
+      "SELECT d FROM Dokumentbeskrivelse d JOIN d.journalpost j WHERE j = :journalpost AND"
+          + " (:pivot IS NULL OR d.id <= :pivot) ORDER BY d.id DESC")
   Page<Dokumentbeskrivelse> paginateDesc(Journalpost journalpost, String pivot, Pageable pageable);
 
   @Query(
@@ -26,7 +27,19 @@ public interface DokumentbeskrivelseRepository extends ArkivBaseRepository<Dokum
   Page<Dokumentbeskrivelse> paginateAsc(Moetesak moetesak, String pivot, Pageable pageable);
 
   @Query(
-      "SELECT d FROM Dokumentbeskrivelse d JOIN d.moetesak j WHERE j = :moetesak AND (:pivot"
+      "SELECT d FROM Moetesak m JOIN m.dokumentbeskrivelse d WHERE m = :moetesak AND (:pivot"
           + " IS NULL OR d.id <= :pivot) ORDER BY d.id DESC")
   Page<Dokumentbeskrivelse> paginateDesc(Moetesak moetesak, String pivot, Pageable pageable);
+
+  @Query(
+      "SELECT d FROM Dokumentbeskrivelse d JOIN d.moetedokument j WHERE j = :moetedokument"
+          + " AND (:pivot IS NULL OR d.id >= :pivot) ORDER BY d.id ASC")
+  Page<Dokumentbeskrivelse> paginateAsc(
+      Moetedokument moetedokument, String pivot, Pageable pageable);
+
+  @Query(
+      "SELECT d FROM Dokumentbeskrivelse d JOIN d.moetedokument j WHERE j = :moetedokument"
+          + " AND (:pivot IS NULL OR d.id <= :pivot) ORDER BY d.id DESC")
+  Page<Dokumentbeskrivelse> paginateDesc(
+      Moetedokument moetedokument, String pivot, Pageable pageable);
 }
