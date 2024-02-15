959b683c91d15ca1a3dd4658e9165812177c209f
Gisle Nes <gisle@gisle.net>
2024-02-15 14:09:33 +0100

    Fix imports

--- src/main/java/no/einnsyn/apiv3/entities/innsynskrav/InnsynskravSenderService.java
+++ src/main/java/no/einnsyn/apiv3/entities/innsynskrav/InnsynskravSenderService.java
@@ -19,6 +19,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.io.ByteArrayResource;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Slf4j
 @Service
