ab048e493fee8037fb9da8afa293a468b559cff9
Gisle Nes <gisle@gisle.net>
2024-03-11 11:50:56 +0100

    Removed update / delete endpoints for InnsynskravDel

--- src/main/java/no/einnsyn/apiv3/entities/innsynskravdel/InnsynskravDelController.java
+++ src/main/java/no/einnsyn/apiv3/entities/innsynskravdel/InnsynskravDelController.java
@@ -9,14 +9,9 @@ import no.einnsyn.apiv3.entities.base.models.BaseGetQueryDTO;
 import no.einnsyn.apiv3.entities.innsynskravdel.models.InnsynskravDelDTO;
 import no.einnsyn.apiv3.error.exceptions.EInnsynException;
 import no.einnsyn.apiv3.validation.existingobject.ExistingObject;
-import no.einnsyn.apiv3.validation.validationgroups.Update;
 import org.springframework.http.ResponseEntity;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -35,25 +30,6 @@ public class InnsynskravDelController {
       @Valid BaseGetQueryDTO query)
       throws EInnsynException {
     var responseBody = service.get(innsynskravDelId, query);
-    return ResponseEntity.ok().body(responseBody);
-  }
-
-  @PutMapping("/innsynskravDel/{innsynskravDelId}")
-  public ResponseEntity<InnsynskravDelDTO> update(
-      @Valid @PathVariable @NotNull @ExistingObject(service = InnsynskravDelService.class)
-          String innsynskravDelId,
-      @RequestBody @Validated(Update.class) InnsynskravDelDTO body)
-      throws EInnsynException {
-    var responseBody = service.update(innsynskravDelId, body);
-    return ResponseEntity.ok().body(responseBody);
-  }
-
-  @DeleteMapping("/innsynskravDel/{innsynskravDelId}")
-  public ResponseEntity<InnsynskravDelDTO> delete(
-      @Valid @PathVariable @NotNull @ExistingObject(service = InnsynskravDelService.class)
-          String innsynskravDelId)
-      throws EInnsynException {
-    var responseBody = service.delete(innsynskravDelId);
     return ResponseEntity.ok().body(responseBody);
   }
 }
