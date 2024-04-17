6d6c1e514e4785f2d0750333934532eefb70ca15
Gisle Nes <gisle@gisle.net>
2024-04-17 12:18:38 +0200

    Suppress warnings for legacy field names

--- src/main/java/no/einnsyn/apiv3/entities/journalpost/models/JournalpostES.java
+++ src/main/java/no/einnsyn/apiv3/entities/journalpost/models/JournalpostES.java
@@ -3,6 +3,7 @@ package no.einnsyn.apiv3.entities.journalpost.models;
 import java.util.List;
 import lombok.Getter;
 import lombok.Setter;
+import no.einnsyn.apiv3.entities.saksmappe.models.SaksmappeES;
 
 @Getter
 @Setter
@@ -10,14 +11,17 @@ public class JournalpostES extends JournalpostDTO {
 
   List<String> type = List.of("Journalpost");
 
+  @SuppressWarnings("java:S116")
   String offentligTittel_SENSITIV;
 
   List<String> avsender;
 
+  @SuppressWarnings("java:S116")
   List<String> avsender_SENSITIV;
 
   List<String> mottaker;
 
+  @SuppressWarnings("java:S116")
   List<String> mottaker_SENSITIV;
 
   List<String> arkivskaperTransitive;
@@ -27,4 +31,6 @@ public class JournalpostES extends JournalpostDTO {
   String arkivskaper;
 
   String arkivskaperSorteringNavn;
+
+  SaksmappeES parent;
 }
