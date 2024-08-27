0fbca4ef481531bade35b1c105e70bab7d71b470
Gisle Nes <gisle@gisle.net>
2024-08-27 11:01:30 +0200

    Implement findByEnhetskode, isDescendantOf in EnhetRepository

--- src/main/java/no/einnsyn/apiv3/entities/enhet/EnhetRepository.java
+++ src/main/java/no/einnsyn/apiv3/entities/enhet/EnhetRepository.java
@@ -11,6 +11,47 @@ public interface EnhetRepository extends BaseRepository<Enhet> {
   Enhet findByOrgnummer(String orgnummer);
 
   @Query(
+      value =
+          """
+          WITH RECURSIVE descendants AS (
+            SELECT e1.*, 1 AS depth
+            FROM enhet e1
+            WHERE e1._id = :rootId
+            UNION ALL
+            SELECT e2.*, d.depth + 1
+            FROM enhet e2
+            INNER JOIN descendants d ON e2.parent_id = d.id
+            WHERE d.depth < 10
+          )
+          SELECT * FROM descendants
+          WHERE enhets_kode ~ CONCAT('(^\\s*|\\s*;\\s*)', :enhetskode, '(\\s*;\\s*|\\s*$)')
+          LIMIT 1;
+          """,
+      nativeQuery = true)
+  Enhet findByEnhetskode(String enhetskode, String rootId);
+
+  @Query(
+      value =
+          """
+          WITH RECURSIVE ancestors AS (
+            SELECT e1._id, e1.id, e1.parent_id, 1 AS depth
+            FROM enhet e1
+            WHERE e1._id = :childId
+            UNION ALL
+            SELECT e2._id, e2.id, e2.parent_id, a.depth + 1
+            FROM enhet e2
+            INNER JOIN ancestors a ON e2.id = a.parent_id
+          )
+          SELECT EXISTS (
+            SELECT 1
+            FROM ancestors
+            WHERE _id = :rootId
+          );
+          """,
+      nativeQuery = true)
+  boolean isAncestorOf(String rootId, String childId);
+
+  @Query(
       "SELECT o FROM Enhet o WHERE o.parent = :parent AND (:pivot IS NULL OR o.id >= :pivot)"
           + " ORDER BY o.id ASC")
   Page<Enhet> paginateAsc(Enhet parent, String pivot, Pageable pageable);
