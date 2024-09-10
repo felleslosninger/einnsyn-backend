783b6108b70fec5b19b48ceba517bc66a383c84d
gunnarsandnes <gunnar.sandnes@gmail.com>
2024-09-10 10:34:50 +0200

    EIN-4708 new index must have a different name

--- src/main/resources/db/migration/V20240910_0800__Drop_constraint_korrpart.sql
+++ src/main/resources/db/migration/V20240910_0800__Drop_constraint_korrpart.sql
@@ -1,4 +1,4 @@
 DROP INDEX IF EXISTS korrespondansepart__external_id_idx;
 
-CREATE INDEX IF NOT EXISTS korrespondansepart__external_id_idx
+CREATE INDEX IF NOT EXISTS korrpart__external_nonunique_id_idx
     on korrespondansepart(_external_id);
