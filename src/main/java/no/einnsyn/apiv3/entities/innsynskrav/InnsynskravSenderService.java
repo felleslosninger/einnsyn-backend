5ec2ee33a78a799e11d55415190a8cfcaedeb959
Gisle Nes <gisle@gisle.net>
2024-09-11 14:32:34 +0200

    Remove `@Async` from sendInnsynskrav (we'll lose the JPA context)

--- src/main/java/no/einnsyn/apiv3/entities/innsynskrav/InnsynskravSenderService.java
+++ src/main/java/no/einnsyn/apiv3/entities/innsynskrav/InnsynskravSenderService.java
@@ -19,7 +19,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.core.io.ByteArrayResource;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -106,7 +105,6 @@ public class InnsynskravSenderService {
    *     removed
    */
   @Transactional
-  @Async
   public void sendInnsynskrav(
       Enhet enhet, Innsynskrav innsynskrav, List<InnsynskravDel> unfilteredInnsynskravDelList) {
     log.trace(
