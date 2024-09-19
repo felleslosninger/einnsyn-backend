5d6fe1a9f27a8633adf56d2c15df93c181d2a3a0
Gisle Nes <gisle@gisle.net>
2024-09-19 10:11:33 +0200

    Sort by standardData, opprettetDato

--- src/main/java/no/einnsyn/apiv3/tasks/elasticsearch/ElasticsearchIdListIterator.java
+++ src/main/java/no/einnsyn/apiv3/tasks/elasticsearch/ElasticsearchIdListIterator.java
@@ -1,21 +1,24 @@
 package no.einnsyn.apiv3.tasks.elasticsearch;
 
 import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.elasticsearch._types.FieldValue;
 import co.elastic.clients.elasticsearch._types.SortOptions;
 import co.elastic.clients.elasticsearch._types.SortOrder;
 import co.elastic.clients.elasticsearch.core.SearchRequest;
 import java.util.Iterator;
 import java.util.List;
 import java.util.NoSuchElementException;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class ElasticsearchIdListIterator implements Iterator<List<String>> {
 
   private final ElasticsearchClient client;
   private final String indexName;
   private final String entityName;
   private final Integer batchSize;
-  private List<String> currentBatch;
-  private List<String> nextBatch;
+  private List<SearchRecord> currentBatch;
+  private List<SearchRecord> nextBatch;
 
   public ElasticsearchIdListIterator(
       ElasticsearchClient client, String indexName, String entityName, Integer batchSize) {
@@ -31,7 +34,7 @@ public class ElasticsearchIdListIterator implements Iterator<List<String>> {
       if (currentBatch == null) {
         nextBatch = fetchNextBatch(null);
       } else {
-        nextBatch = fetchNextBatch(currentBatch.get(currentBatch.size() - 1));
+        nextBatch = fetchNextBatch(currentBatch.get(currentBatch.size() - 1).getSort());
       }
     }
     return !nextBatch.isEmpty();
@@ -44,15 +47,21 @@ public class ElasticsearchIdListIterator implements Iterator<List<String>> {
     }
     currentBatch = nextBatch;
     nextBatch = null;
-    return currentBatch;
+    return currentBatch.stream().map(SearchRecord::getId).toList();
   }
 
-  private List<String> fetchNextBatch(String searchAfter) {
+  private List<SearchRecord> fetchNextBatch(List<FieldValue> searchAfter) {
     // Create a search request
     var requestBuilder = new SearchRequest.Builder();
     requestBuilder.index(indexName);
-    requestBuilder.query(q -> q.term(t -> t.field("type").value(entityName)));
-    requestBuilder.sort(SortOptions.of(so -> so.field(f -> f.field("_id").order(SortOrder.Asc))));
+    requestBuilder.query(
+        q ->
+            q.terms(
+                t -> t.field("type").terms(te -> te.value(List.of(FieldValue.of(entityName))))));
+    requestBuilder.sort(
+        SortOptions.of(so -> so.field(f -> f.field("standardDato").order(SortOrder.Asc))));
+    requestBuilder.sort(
+        SortOptions.of(so -> so.field(f -> f.field("opprettetDato").order(SortOrder.Asc))));
     requestBuilder.source(s -> s.fetch(false));
     requestBuilder.size(batchSize);
 
@@ -63,9 +72,32 @@ public class ElasticsearchIdListIterator implements Iterator<List<String>> {
     try {
       var searchRequest = requestBuilder.build();
       var searchResponse = client.search(searchRequest, Void.class);
-      return searchResponse.hits().hits().stream().map(h -> h.id()).toList();
+      return searchResponse.hits().hits().stream()
+          .map(
+              h -> {
+                return new SearchRecord(h.id(), h.sort());
+              })
+          .toList();
     } catch (Exception e) {
       throw new RuntimeException("Failed to fetch next batch: " + e.getMessage(), e);
+    }
+  }
+
+  private class SearchRecord {
+    private final String id;
+    private final List<FieldValue> sort;
+
+    public SearchRecord(String id, List<FieldValue> sort) {
+      this.id = id;
+      this.sort = sort;
+    }
+
+    public String getId() {
+      return id;
+    }
+
+    public List<FieldValue> getSort() {
+      return sort;
     }
   }
 }
