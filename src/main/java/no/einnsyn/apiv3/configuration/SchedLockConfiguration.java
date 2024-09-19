9d2b35427c6c36f51ea0856ca6f49aed71e163cb
Gisle Nes <gisle@gisle.net>
2024-09-19 10:09:07 +0200

    SchedulerConfiguration -> SchedLockConfiguration

--- src/main/java/no/einnsyn/apiv3/configuration/SchedLockConfiguration.java
+++ src/main/java/no/einnsyn/apiv3/configuration/SchedLockConfiguration.java
@@ -4,13 +4,18 @@ import javax.sql.DataSource;
 import net.javacrumbs.shedlock.core.LockProvider;
 import net.javacrumbs.shedlock.provider.jdbctemplate.JdbcTemplateLockProvider;
 import net.javacrumbs.shedlock.spring.annotation.EnableSchedulerLock;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.core.JdbcTemplate;
 
+@ConditionalOnProperty(
+    value = "application.schedlock.disable",
+    havingValue = "false",
+    matchIfMissing = true)
 @Configuration
 @EnableSchedulerLock(defaultLockAtMostFor = "PT30S")
-public class SchedulerConfiguration {
+public class SchedLockConfiguration {
   @Bean
   public LockProvider lockProvider(DataSource dataSource) {
     return new JdbcTemplateLockProvider(
