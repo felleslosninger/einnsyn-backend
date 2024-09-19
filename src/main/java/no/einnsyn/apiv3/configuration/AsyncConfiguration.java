91f6b7a92fdfe0bce18c99acfc1df5454b60404d
Gisle Nes <gisle@gisle.net>
2024-09-19 10:49:04 +0200

    New strategy for BlockingQueue

--- src/main/java/no/einnsyn/apiv3/configuration/AsyncConfiguration.java
+++ src/main/java/no/einnsyn/apiv3/configuration/AsyncConfiguration.java
@@ -1,5 +1,7 @@
 package no.einnsyn.apiv3.configuration;
 
+import java.util.concurrent.RejectedExecutionException;
+import java.util.concurrent.RejectedExecutionHandler;
 import java.util.concurrent.ThreadPoolExecutor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
@@ -19,10 +21,22 @@ public class AsyncConfiguration {
     executor.setCorePoolSize(1);
     executor.setMaxPoolSize(indexConcurrency);
     executor.setQueueCapacity(1000);
-    executor.setRejectedExecutionHandler(
-        new ThreadPoolExecutor.CallerRunsPolicy()); // Block the calling thread when queue is full
+    executor.setRejectedExecutionHandler(new BlockingQueuePolicy()); // Custom blocking handler
     executor.setThreadNamePrefix("Index-");
     executor.initialize();
     return executor;
+  }
+
+  class BlockingQueuePolicy implements RejectedExecutionHandler {
+    @Override
+    public void rejectedExecution(Runnable r, ThreadPoolExecutor executor) {
+      try {
+        executor.getQueue().put(r);
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        throw new RejectedExecutionException(
+            "Task was interrupted while waiting to be enqueued", e);
+      }
+    }
   }
 }
