5e517149318efed5daa2842240475b9cb8f17e88
Gisle Nes <gisle@gisle.net>
2024-10-08 13:05:30 +0200

    Support variables in template strings

--- src/main/java/no/einnsyn/apiv3/utils/MailRenderer.java
+++ src/main/java/no/einnsyn/apiv3/utils/MailRenderer.java
@@ -37,7 +37,19 @@ public class MailRenderer {
    * @param context
    * @return
    */
-  public String render(String templateName, Map<String, Object> context) {
+  public String renderFile(String templateName, Map<String, Object> context) {
+    // Get correct template for language, and render it
+    var template = getTemplate(templateName);
+    var writer = new StringWriter();
+    return template.execute(writer, context).toString();
+  }
+
+  /**
+   * @param templateName
+   * @param context
+   * @return
+   */
+  public String renderString(String template, Map<String, Object> context) {
     // Get correct template for language, and render it
     var template = getTemplate(templateName);
     var writer = new StringWriter();
