86e73b59f102562725a8444f419ae580df88508c
Gisle Nes <gisle@gisle.net>
2024-10-16 08:38:10 +0200

    Reformat SQL

--- src/main/java/no/einnsyn/apiv3/entities/lagretsoek/LagretSoekRepository.java
+++ src/main/java/no/einnsyn/apiv3/entities/lagretsoek/LagretSoekRepository.java
@@ -18,28 +18,31 @@ public interface LagretSoekRepository extends BaseRepository<LagretSoek> {
   LagretSoek findByLegacyId(UUID legacyId);
 
   @Query(
-      ""
-          + "SELECT e FROM LagretSoek e WHERE "
-          + "subscribe = true AND "
-          + "hitCount > 0 AND "
-          + "bruker.id = :brukerId")
+      """
+      SELECT e FROM LagretSoek e WHERE
+      subscribe = true AND
+      hitCount > 0 AND
+      bruker.id = :brukerId
+      """)
   Stream<LagretSoek> findLagretSoekWithHitsByBruker(String brukerId);
 
   @Query(
-      ""
-          + "SELECT DISTINCT e.bruker.id FROM LagretSoek e WHERE "
-          + "subscribe = true AND "
-          + "hitCount > 0")
+      """
+      SELECT DISTINCT e.bruker.id FROM LagretSoek e WHERE
+      subscribe = true AND
+      hitCount > 0
+      """)
   Stream<String> findBrukerWithLagretSoekHits();
 
   @Query(
       value =
-          ""
-              + "UPDATE lagret_sok "
-              + "SET hit_count = hit_count + 1 "
-              + "WHERE id = :legacyId "
-              // + "AND abonnere = true "
-              + "RETURNING hit_count",
+          """
+          UPDATE lagret_sok
+          SET hit_count = hit_count + 1
+          WHERE id = :legacyId
+          AND abonnere = true
+          RETURNING hit_count
+          """,
       nativeQuery = true)
   @Transactional(propagation = Propagation.REQUIRES_NEW)
   Integer addHitByLegacyId(UUID legacyId);
@@ -55,16 +58,20 @@ public interface LagretSoekRepository extends BaseRepository<LagretSoek> {
   void deleteHits(List<String> idList);
 
   @Query(
-      "SELECT o FROM LagretSoek o "
-          + "WHERE bruker = :bruker "
-          + "AND (:pivot IS NULL OR id >= :pivot) "
-          + "ORDER BY id ASC")
+      """
+      SELECT o FROM LagretSoek o
+      WHERE bruker = :bruker
+      AND (:pivot IS NULL OR id >= :pivot)
+      ORDER BY id ASC
+      """)
   Page<LagretSoek> paginateAsc(Bruker bruker, String pivot, Pageable pageable);
 
   @Query(
-      "SELECT o FROM LagretSoek o "
-          + "WHERE bruker = :bruker "
-          + "AND (:pivot IS NULL OR id <= :pivot) "
-          + "ORDER BY id DESC")
+      """
+      SELECT o FROM LagretSoek o
+      WHERE bruker = :bruker
+      AND (:pivot IS NULL OR id <= :pivot)
+      ORDER BY id DESC
+      """)
   Page<LagretSoek> paginateDesc(Bruker bruker, String pivot, Pageable pageable);
 }
