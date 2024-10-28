82e5946ce8c452528ccfc4ccbf0297bb495ec353
Håvard Tørresen <hto@digdir.no>
2024-10-28 16:19:51 +0100

    EIN-4752: Added support for RSA-keys, based on admin-api

--- src/main/java/no/einnsyn/apiv3/authentication/bruker/JwtService.java
+++ src/main/java/no/einnsyn/apiv3/authentication/bruker/JwtService.java
@@ -4,12 +4,23 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
+import java.io.IOException;
+import java.io.StringReader;
+import java.security.*;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.RSAPrivateKeySpec;
+import java.security.spec.RSAPublicKeySpec;
 import java.util.Date;
 import java.util.Map;
 import java.util.UUID;
 import javax.crypto.SecretKey;
 import lombok.Getter;
 import no.einnsyn.apiv3.authentication.bruker.models.BrukerUserDetails;
+import org.bouncycastle.crypto.params.RSAKeyParameters;
+import org.bouncycastle.crypto.util.PrivateKeyFactory;
+import org.bouncycastle.crypto.util.PublicKeyFactory;
+import org.bouncycastle.openssl.PEMKeyPair;
+import org.bouncycastle.openssl.PEMParser;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -19,12 +30,17 @@ public class JwtService {
   @Value("${application.jwt.encryption-secret}")
   private String secret;
 
+  @Value("${application.jwt.isRSA}")
+  private boolean keyIsRSA;
+
   @Getter
   @Value("${application.jwt.accessTokenExpiration}")
   private long expiration;
 
   @Value("${application.jwt.refreshTokenExpiration}")
   private long refreshExpiration;
+
+  private KeyPair keyPair;
 
   public String getUsername(String token) {
     var claims = extractAllClaims(token);
@@ -53,7 +69,15 @@ public class JwtService {
   }
 
   public Claims extractAllClaims(String token) {
-    return Jwts.parser().verifyWith(getSecretKey()).build().parseSignedClaims(token).getPayload();
+    if (keyIsRSA) {
+      return Jwts.parser()
+          .verifyWith(getKeyPair().getPublic())
+          .build()
+          .parseSignedClaims(token)
+          .getPayload();
+    } else {
+      return Jwts.parser().verifyWith(getSecretKey()).build().parseSignedClaims(token).getPayload();
+    }
   }
 
   public String generateToken(BrukerUserDetails userDetails) {
@@ -66,7 +90,7 @@ public class JwtService {
 
   public String generateToken(
       Map<String, Object> extraClaims, BrukerUserDetails userDetails, long expiration) {
-    var secretKey = getSecretKey();
+    var secretKey = keyIsRSA ? getKeyPair().getPrivate() : getSecretKey();
 
     return Jwts.builder()
         .claims(extraClaims)
@@ -81,5 +105,33 @@ public class JwtService {
   public SecretKey getSecretKey() {
     byte[] secretBytes = Decoders.BASE64.decode(secret);
     return Keys.hmacShaKeyFor(secretBytes);
+  }
+
+  private KeyPair getKeyPair() {
+    if (keyPair == null) {
+      try {
+        var pemParser = new PEMParser(new StringReader(secret.replace("\\n", "\n")));
+        var secretKey = (PEMKeyPair) pemParser.readObject();
+        pemParser.close();
+
+        var privateSpec =
+            (RSAKeyParameters) PrivateKeyFactory.createKey(secretKey.getPrivateKeyInfo());
+        var publicSpec =
+            (RSAKeyParameters) PublicKeyFactory.createKey(secretKey.getPublicKeyInfo());
+        KeyFactory kf = KeyFactory.getInstance("RSA");
+        PrivateKey privateKey =
+            kf.generatePrivate(
+                new RSAPrivateKeySpec(privateSpec.getModulus(), privateSpec.getExponent()));
+        PublicKey publicKey =
+            kf.generatePublic(
+                new RSAPublicKeySpec(publicSpec.getModulus(), publicSpec.getExponent()));
+
+        keyPair = new KeyPair(publicKey, privateKey);
+      } catch (IOException | NoSuchAlgorithmException | InvalidKeySpecException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    return keyPair;
   }
 }
