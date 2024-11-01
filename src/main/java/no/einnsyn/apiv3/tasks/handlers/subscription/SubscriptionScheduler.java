779946f49ca8dc9521c6e1f7b12d6e44e189917f
Gisle Nes <gisle@gisle.net>
2024-11-01 15:40:30 +0100

    Change LagretSoekSak schedule variables

--- src/main/java/no/einnsyn/apiv3/tasks/handlers/subscription/SubscriptionScheduler.java
+++ src/main/java/no/einnsyn/apiv3/tasks/handlers/subscription/SubscriptionScheduler.java
@@ -47,7 +47,7 @@ public class SubscriptionScheduler {
   }
 
   // Notify lagretSak every ten minutes
-  @Scheduled(cron = "${application.lagretsak.notificationSchedule:0 */10 * * * *}")
+  @Scheduled(cron = "${application.lagretSak.notificationSchedule:0 */10 * * * *}")
   @SchedulerLock(name = "NotifyLagretSak", lockAtLeastFor = "5m", lockAtMostFor = "5m")
   @Transactional(readOnly = true)
   public void notifyLagretSak() {
@@ -65,7 +65,7 @@ public class SubscriptionScheduler {
 
   // Notify lagretSoek daily
   @Scheduled(
-      cron = "${application.lagretsoek.notificationSchedule:0 0 6 * * *}",
+      cron = "${application.lagretSoek.notificationSchedule:0 0 6 * * *}",
       zone = "Europe/Oslo")
   @SchedulerLock(name = "NotifyLagretSoek", lockAtLeastFor = "10m", lockAtMostFor = "10m")
   @Transactional(readOnly = true)
