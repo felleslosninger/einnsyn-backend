3714ea40c91772a4a8353d3df57cc05bb0633524
Gisle Nes <gisle@gisle.net>
2024-11-18 14:17:58 +0100

    Allow other protocols than http/https in IRIs

--- src/main/java/no/einnsyn/apiv3/utils/IRIMatcher.java
+++ src/main/java/no/einnsyn/apiv3/utils/IRIMatcher.java
@@ -2,10 +2,21 @@ package no.einnsyn.apiv3.utils;
 
 import java.util.regex.Pattern;
 
+/**
+ * The {@code IRIMatcher} class provides a utility method to check if a given string is a valid IRI.
+ */
 public class IRIMatcher {
 
-  private static Pattern pattern = Pattern.compile("^https?://.*", Pattern.CASE_INSENSITIVE);
+  // A pre-compiled pattern that should match all IRIs
+  private static Pattern pattern =
+      Pattern.compile("^[a-zA-Z][a-zA-Z+.-]*://[a-zA-Z0-9]+", Pattern.CASE_INSENSITIVE);
 
+  /**
+   * Checks if the provided string is an IRI.
+   *
+   * @param iri the string to be checked against the IRI pattern
+   * @return {@code true} if the string matches the IRI pattern, {@code false} otherwise
+   */
   public static boolean matches(String iri) {
     return pattern.matcher(iri).matches();
   }
