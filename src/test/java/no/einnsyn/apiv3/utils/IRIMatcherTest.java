4919651bd8926dfb6b948df0920d88a63af2dbf3
Gisle Nes <gisle@gisle.net>
2024-11-22 10:08:32 +0100

    Remove SpringBootTest

--- src/test/java/no/einnsyn/apiv3/utils/IRIMatcherTest.java
+++ src/test/java/no/einnsyn/apiv3/utils/IRIMatcherTest.java
@@ -1,9 +1,7 @@
 package no.einnsyn.apiv3.utils;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class IRIMatcherTest {
 
   @Test
