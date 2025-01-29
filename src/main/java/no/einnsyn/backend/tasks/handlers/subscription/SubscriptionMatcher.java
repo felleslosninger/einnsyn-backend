a255212a83b2826174910b0cb1d807e6e1cad79d
Gisle Nes <gisle@gisle.net>
2025-01-29 09:52:10 +0100

    Keep non-accessible documents in ES, for admins to search

--- src/main/java/no/einnsyn/backend/tasks/handlers/subscription/SubscriptionMatcher.java
+++ src/main/java/no/einnsyn/backend/tasks/handlers/subscription/SubscriptionMatcher.java
@@ -8,11 +8,20 @@ import java.io.StringReader;
 import java.util.List;
 import java.util.UUID;
 import lombok.extern.slf4j.Slf4j;
+import no.einnsyn.backend.common.indexable.Indexable;
+import no.einnsyn.backend.entities.arkivbase.models.ArkivBaseES;
 import no.einnsyn.backend.entities.base.models.BaseES;
+import no.einnsyn.backend.entities.enhet.EnhetService;
+import no.einnsyn.backend.entities.journalpost.JournalpostService;
+import no.einnsyn.backend.entities.journalpost.models.JournalpostES;
 import no.einnsyn.backend.entities.lagretsak.LagretSakRepository;
 import no.einnsyn.backend.entities.lagretsoek.LagretSoekService;
 import no.einnsyn.backend.entities.mappe.models.MappeES;
+import no.einnsyn.backend.entities.moetemappe.MoetemappeService;
 import no.einnsyn.backend.entities.moetemappe.models.MoetemappeES;
+import no.einnsyn.backend.entities.moetesak.MoetesakService;
+import no.einnsyn.backend.entities.moetesak.models.MoetesakES;
+import no.einnsyn.backend.entities.saksmappe.SaksmappeService;
 import no.einnsyn.backend.entities.saksmappe.models.SaksmappeES;
 import no.einnsyn.backend.tasks.events.IndexEvent;
 import no.einnsyn.backend.utils.ElasticsearchIterator;
@@ -24,16 +33,31 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class SubscriptionMatcher {
 
-  private LagretSakRepository lagretSakRepository;
-  private LagretSoekService lagretSoekService;
-  private ElasticsearchClient esClient;
-  private Gson gson;
+  private final EnhetService enhetService;
+  private final SaksmappeService saksmappeService;
+  private final JournalpostService journalpostService;
+  private final MoetemappeService moetemappeService;
+  private final MoetesakService moetesakService;
+  private final LagretSakRepository lagretSakRepository;
+  private final LagretSoekService lagretSoekService;
+  private final ElasticsearchClient esClient;
+  private final Gson gson;
 
   public SubscriptionMatcher(
+      EnhetService enhetService,
+      SaksmappeService saksmappeService,
+      JournalpostService journalpostService,
+      MoetemappeService moetemappeService,
+      MoetesakService moetesakService,
       LagretSakRepository lagretSakRepository,
       LagretSoekService lagretSoekService,
       ElasticsearchClient esClient,
       Gson gson) {
+    this.enhetService = enhetService;
+    this.saksmappeService = saksmappeService;
+    this.journalpostService = journalpostService;
+    this.moetemappeService = moetemappeService;
+    this.moetesakService = moetesakService;
     this.lagretSakRepository = lagretSakRepository;
     this.lagretSoekService = lagretSoekService;
     this.esClient = esClient;
@@ -45,15 +69,21 @@ public class SubscriptionMatcher {
   public void handleIndex(IndexEvent event) {
     var document = event.getDocument();
 
-    // Check if arkivskaperTransitive is hidden
+    if (document instanceof ArkivBaseES arkivBaseDocument) {
 
-    if (document instanceof MappeES mappeDocument) {
-      handleSak(mappeDocument);
-    }
+      // TODO: Uncomment this when PR with ArkivBaseES.adminstrativEnhet is merged
+      // if (enhetService.isSkjult(arkivBaseDocument.getAdministrativEnhet())) {
+      //   return;
+      // }
 
-    // Match saved searches only for inserts
-    if (event.isInsert()) {
-      handleSearch(document);
+      if (document instanceof MappeES mappeDocument) {
+        handleSak(mappeDocument);
+      }
+
+      // Match saved searches only
+      if (event.isInsert() || turnedAccessible(document)) {
+        handleSearch(document);
+      }
     }
   }
 
@@ -100,5 +130,29 @@ public class SubscriptionMatcher {
       var uuid = UUID.fromString(hit.id());
       lagretSoekService.addHit(document, uuid);
     }
+  }
+
+  /**
+   * If document's "accessibleAfter" property is after "lastIndexed", this reindex will make it
+   * accessible, and subscribers should be notified.
+   *
+   * @param document
+   * @return
+   */
+  private boolean turnedAccessible(BaseES document) {
+    var id = document.getId();
+    var object =
+        switch (document) {
+          case SaksmappeES saksmappe -> saksmappeService.findById(id);
+          case JournalpostES journalpost -> journalpostService.findById(id);
+          case MoetemappeES moetemappe -> moetemappeService.findById(id);
+          case MoetesakES moetesak -> moetesakService.findById(id);
+          default -> null;
+        };
+    if (object != null
+        && object.getAccessibleAfter().isAfter(((Indexable) document).getLastIndexed())) {
+      return true;
+    }
+    return false;
   }
 }
