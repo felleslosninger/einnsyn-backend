ea16143c07786522c7bb0d5d98d2f8c45affddab
Gisle Nes <gisle@gisle.net>
2025-01-31 09:54:16 +0100

    Don't enable accessibleFilters more than once per transaction

--- src/main/java/no/einnsyn/backend/configuration/AccessibleFilterAspect.java
+++ src/main/java/no/einnsyn/backend/configuration/AccessibleFilterAspect.java
@@ -8,15 +8,22 @@ import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.hibernate.Session;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
 /**
  * Aspect that will enable / disable the "accessibleAfter" filters for entities based on the
  * authentication status of the user.
+ *
+ * <p>This will be checked before each transactional method is executed.
  */
 @Aspect
 @Component
 @Slf4j
+// Execute after @Transactional, so filters are enabled *inside* the transaction:
+@Order(Ordered.LOWEST_PRECEDENCE)
 public class AccessibleFilterAspect {
 
   AuthenticationService authenticationService;
@@ -32,8 +39,17 @@ public class AccessibleFilterAspect {
     this.enhetService = enhetService;
   }
 
+  /**
+   * Enable / disable filters based on the authentication status of the user, at the start of each
+   * transaction.
+   */
   @Before("@annotation(org.springframework.transaction.annotation.Transactional)")
   public void enableFilters(JoinPoint joinPoint) throws Throwable {
+    // Ensure logic runs only when a new transaction starts
+    if (!TransactionSynchronizationManager.isSynchronizationActive()) {
+      return;
+    }
+
     var session = entityManager.unwrap(Session.class);
 
     // Remove filter for admins
