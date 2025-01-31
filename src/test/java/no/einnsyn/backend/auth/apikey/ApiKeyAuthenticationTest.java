c749c1436990124dcf6a1bd55649e0d2b4f8ab7e
Gisle Nes <gisle@gisle.net>
2025-01-31 12:36:39 +0100

    Implemented ACTING-AS header

--- src/test/java/no/einnsyn/backend/auth/apikey/ApiKeyAuthenticationTest.java
+++ src/test/java/no/einnsyn/backend/auth/apikey/ApiKeyAuthenticationTest.java
@@ -5,10 +5,14 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.google.gson.JsonParser;
 import no.einnsyn.backend.EinnsynControllerTestBase;
 import no.einnsyn.backend.entities.apikey.models.ApiKeyDTO;
+import no.einnsyn.backend.entities.arkiv.models.ArkivDTO;
+import no.einnsyn.backend.entities.arkivdel.models.ArkivdelDTO;
 import no.einnsyn.backend.entities.enhet.models.EnhetDTO;
+import no.einnsyn.backend.entities.saksmappe.models.SaksmappeDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.ActiveProfiles;
 
@@ -49,5 +53,49 @@ class ApiKeyAuthenticationTest extends EinnsynControllerTestBase {
 
     deleteAdmin("/enhet/" + enhetDTO.getId());
     deleteAdmin("/enhet/" + enhetDTO2.getId());
+  }
+
+  @Test
+  void testActingAs() throws Exception {
+    var headers = getActingAsHeaders(journalenhetKey, underenhetId);
+
+    // Create arkiv & arkivdel
+    var response = post("/arkiv", getArkivJSON());
+    assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    var arkivDTO = gson.fromJson(response.getBody(), ArkivDTO.class);
+
+    response = post("/arkiv/" + arkivDTO.getId() + "/arkivdel", getArkivdelJSON());
+    assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    var arkivdelDTO = gson.fromJson(response.getBody(), ArkivdelDTO.class);
+
+    // Publish as underenhet
+    response = post("/arkivdel/" + arkivdelDTO.getId() + "/saksmappe", getSaksmappeJSON(), headers);
+    assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    var saksmappeDTO = gson.fromJson(response.getBody(), SaksmappeDTO.class);
+    assertEquals(underenhetId, saksmappeDTO.getAdministrativEnhetObjekt().getId());
+
+    // Publish as journalenhet
+    response =
+        post(
+            "/arkivdel/" + arkivdelDTO.getId() + "/saksmappe", getSaksmappeJSON(), journalenhetKey);
+    assertEquals(HttpStatus.CREATED, response.getStatusCode());
+    var saksmappeDTO2 = gson.fromJson(response.getBody(), SaksmappeDTO.class);
+    assertEquals(journalenhetId, saksmappeDTO2.getAdministrativEnhetObjekt().getId());
+
+    // Can not delete journalenhet's enhet's saksmappe when acting as underenhet
+    response = delete("/saksmappe/" + saksmappeDTO2.getId(), headers);
+    assertEquals(HttpStatus.FORBIDDEN, response.getStatusCode());
+
+    // Can delete underenhet's saksmappe when acting as underenhet
+    response = delete("/saksmappe/" + saksmappeDTO.getId(), headers);
+    assertEquals(HttpStatus.OK, response.getStatusCode());
+
+    response = delete("/arkiv/" + arkivDTO.getId());
+  }
+
+  private HttpHeaders getActingAsHeaders(String authKey, String actingAsId) {
+    var headers = getAuthHeaders(authKey);
+    headers.add("ACTING-AS", actingAsId);
+    return headers;
   }
 }
