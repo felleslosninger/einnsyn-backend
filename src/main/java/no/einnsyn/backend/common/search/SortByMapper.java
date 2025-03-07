f6f72dc2b98ba534723d051bcae8c32a7a14361d
Gisle Nes <gisle@gisle.net>
2025-03-07 14:23:04 +0100

    Update sortBy mapping

--- src/main/java/no/einnsyn/backend/common/search/SortByMapper.java
+++ src/main/java/no/einnsyn/backend/common/search/SortByMapper.java
@@ -1,14 +1,14 @@
 package no.einnsyn.backend.common.search;
 
 import co.elastic.clients.elasticsearch._types.FieldValue;
-import java.time.Instant;
 
 public class SortByMapper {
 
   public static String resolve(String sortBy) {
     return switch (sortBy) {
-      case "id" -> "id";
       case "score" -> "_score";
+      case "id" -> "id";
+      case "entity" -> "type";
       case "publisertDato" -> "publisertDato";
       case "oppdatertDato" -> "oppdatertDato";
       case "moetedato" -> "moetedato";
@@ -20,11 +20,12 @@ public class SortByMapper {
 
   public static FieldValue toFieldValue(String sortBy, String value) {
     return switch (sortBy) {
-      case "id" -> FieldValue.of(value);
       case "score" -> FieldValue.of(Double.parseDouble(value));
-      case "publisertDato" -> FieldValue.of(Instant.parse(value));
-      case "oppdatertDato" -> FieldValue.of(Instant.parse(value));
-      case "moetedato" -> FieldValue.of(Instant.parse(value));
+      case "id" -> FieldValue.of(value);
+      case "entity" -> FieldValue.of(value);
+      case "publisertDato" -> FieldValue.of(Long.parseLong(value));
+      case "oppdatertDato" -> FieldValue.of(Long.parseLong(value));
+      case "moetedato" -> FieldValue.of(Long.parseLong(value));
       case "fulltekst" -> FieldValue.of(Boolean.parseBoolean(value));
       case "type" -> FieldValue.of(value);
       default -> null;
