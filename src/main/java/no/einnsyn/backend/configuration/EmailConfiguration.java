39fc3ac5545e2e5ab42ef3783e3fbe179413265a
Gisle Nes <gisle@gisle.net>
2025-10-23 19:30:24 +0200

    Add (safe) cast to JavaMailServerImpl

--- src/main/java/no/einnsyn/backend/configuration/EmailConfiguration.java
+++ src/main/java/no/einnsyn/backend/configuration/EmailConfiguration.java
@@ -3,6 +3,7 @@ package no.einnsyn.backend.configuration;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 
 @Configuration
@@ -33,7 +34,7 @@ public class EmailConfiguration {
   private String debug;
 
   @Bean
-  JavaMailSenderImpl getJavaMailSender() {
+  JavaMailSender getJavaMailSender() {
     var mailSender = new JavaMailSenderImpl();
 
     mailSender.setHost(mailServerHost);
