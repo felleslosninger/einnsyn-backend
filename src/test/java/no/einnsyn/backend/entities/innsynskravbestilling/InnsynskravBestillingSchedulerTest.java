fe926fccb94961e0075dcab248e0908d44fd1195
Håvard Tørresen <hto@digdir.no>
2025-11-13 09:50:07 +0100

    EIN-4567: Further expansion of test

--- src/test/java/no/einnsyn/backend/entities/innsynskravbestilling/InnsynskravBestillingSchedulerTest.java
+++ src/test/java/no/einnsyn/backend/entities/innsynskravbestilling/InnsynskravBestillingSchedulerTest.java
@@ -8,10 +8,12 @@ import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+import com.google.gson.internal.LinkedTreeMap;
 import jakarta.mail.internet.MimeMessage;
 import java.time.temporal.ChronoUnit;
 import java.util.concurrent.TimeUnit;
 import no.einnsyn.backend.EinnsynLegacyElasticTestBase;
+import no.einnsyn.backend.common.responses.models.PaginatedList;
 import no.einnsyn.backend.entities.arkiv.models.ArkivDTO;
 import no.einnsyn.backend.entities.arkivdel.models.ArkivdelDTO;
 import no.einnsyn.backend.entities.innsynskravbestilling.models.InnsynskravBestillingDTO;
@@ -527,6 +529,19 @@ class InnsynskravBestillingSchedulerTest extends EinnsynLegacyElasticTestBase {
     taskTestService.updateOutdatedDocuments();
     awaitSideEffects();
     captureIndexedDocuments(2);
+
+    // Receiving org should still be able to see both Innsynskrav
+    var innsynskravListResponse = get("/enhet/" + journalenhetId + "/innsynskrav");
+    assertEquals(HttpStatus.OK, innsynskravListResponse.getStatusCode());
+    var innsynskravList = gson.fromJson(innsynskravListResponse.getBody(), PaginatedList.class);
+    assertNotNull(innsynskravList);
+    assertEquals(2, innsynskravList.getItems().size());
+    for (var item : innsynskravList.getItems()) {
+      if (item instanceof LinkedTreeMap<?, ?> mapItem) {
+        var itemResponse = get("/innsynskrav/" + mapItem.get("id"));
+        assertEquals(HttpStatus.OK, itemResponse.getStatusCode());
+      }
+    }
 
     // Cleanup...
     // Delete InnsynskravBestilling
