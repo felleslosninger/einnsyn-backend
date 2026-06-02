0716995bd36ce6913d5a343d6907848844ae6809
Daniel Lerum <daniel.lerum@digdir.no>
2026-06-02 14:59:55 +0200

    EIN-4963 minor fix spotless

--- src/test/java/no/einnsyn/backend/entities/matrikkelnummer/MatrikkelnummerTest.java
+++ src/test/java/no/einnsyn/backend/entities/matrikkelnummer/MatrikkelnummerTest.java
@@ -23,7 +23,6 @@ import org.json.JSONObject;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
