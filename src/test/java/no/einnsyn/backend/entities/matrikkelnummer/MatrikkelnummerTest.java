42d710f9e6110ea97685b2a0e746ca4c8cbaa204
Daniel Lerum <daniel.lerum@digdir.no>
2026-06-09 11:12:41 +0200

    EIN-4963 spotless

--- src/test/java/no/einnsyn/backend/entities/matrikkelnummer/MatrikkelnummerTest.java
+++ src/test/java/no/einnsyn/backend/entities/matrikkelnummer/MatrikkelnummerTest.java
@@ -3,7 +3,6 @@ package no.einnsyn.backend.entities.matrikkelnummer;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
